147aed201e2b1f1108d46ceb06e7a31889b8afef
mariameza99 <54654158+mariameza99@users.noreply.github.com>
2020-10-01 09:38:18 -0600

    Implementación de Backend

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php 
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	if(!isset($_SESSION)) {
+		session_start();
+	}
+
+	if(!defined('BASE_PATH')){
+		define('BASE_PATH', "")
+	}
+
+?>
---
+++ userController.php
@@ -0,0 +1,48 @@
+<?php 
+	
+	include_once "connectionController.php";
+
+	class UserController
+	{
+
+		function get()
+		{
+			$conn = connect();
+			if(!$conn->connect_error){
+
+				$query = "select * from users";
+				$prepared_query = $sconn->prepare($query);
+				$prepared_query->execute();
+
+				$results = $prepared_query->get_result();
+				$users = $results->fetch_all(MYSQLI_ASSOC);
+
+				return $users;
+			}else{
+				return array();
+			}
+		}
+
+		function store($name,$last,$email,$pass){
+			$conn = connect();
+			if (!$conn->connect_error){
+				if($name!="" && $last!="" && $email!="" && $pass!="")
+					$query = "insert into users (name,lastname,email,password) values (?,?,?,?)";
+				$prepared_query = $conn->prepare($query);
+				$prepared_query->bind_param('ssss',$name,$last,$email,$pass);
+				if($prepared_query->execute()){
+					$_SESSION['message'] = "Registro creado exitosamente";
+					$_SESSION['status'] = "Success";
+					header("Location: ".$_SERVER['HTTP_REFERER']);
+				}else{
+					$_SESSION['message'] = "Problemas con la conexión al servidor";
+					$_SESSION['status'] = "error";
+					header("Location: ".$_SERVER['HTTP_REFERER']);
+				}
+			}
+		}
+
+	}
+
+
+ ?>
